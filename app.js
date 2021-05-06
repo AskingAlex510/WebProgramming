127203d781d82f496248159216271352600fbb2d
s3877327 <s3877327@rmit.edu.vn>
2021-05-06 13:40:11 +0700

    finish the log in and rework on the account page

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ function popup() {
     sessionStorage.setItem("consent", "True");
     document.getElementById("consentPopup").style.display = 'none';
 }
+
+// keep the login status
+function loggedStatus() {
+    var key = localStorage.getItem("log-mail");
+    if (key === null) {
+        window.open("login.html");
+    }
+    else {
+        window.open("account.html");
+    }
+}
