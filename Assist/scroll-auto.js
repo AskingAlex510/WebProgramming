c3cdc9bbb0f10cad7fe0090fbbd6f326b07ef525
PapaPanda <nguyennhatquang0205@gmail.com>
2021-05-07 13:05:35 +0700

    nearly done for scrolling

--- Assist/scroll-auto.js
+++ Assist/scroll-auto.js
@@ -1,14 +1,4 @@
-var my = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementById("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+function myFunction(){
+  var element = document.getElementsByClassName("scroll_content")
+  window.scrollTo(1000 ,0);
 }
