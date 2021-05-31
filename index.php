d61272b3755e850bf9efbe2007fa90a563cfb405
PapaPanda <nguyennhatquang0205@gmail.com>
2021-05-31 18:20:32 +0700

    done with the php 07

--- index.php
+++ index.php
@@ -17,19 +17,19 @@
     <div id="topnav">
     <nav>
          <ul>
-            <li><a href="order.html">ORDERS</a></li>
-            <li><a href="contacts.html">CONTACTS</a></li>
-            <li><a href="faqs.html">FAQS</a></li>
+            <li><a href="order.php">ORDERS</a></li>
+            <li><a href="contacts.php">CONTACTS</a></li>
+            <li><a href="faqs.php">FAQS</a></li>
             <li class="dropdown">
                 <a class="dropbutton">BROWSE</a>
                 <div class="drop-content">
-                    <a href="browse_name.html">Browse Stores by Names</a>
-                    <a href="browse_cat.html">Browse Stores by Category </a>
+                    <a href="browse_name.php">Browse Stores by Names</a>
+                    <a href="browse_cat.php">Browse Stores by Category </a>
                 </div>
             </li>
             <li><a onclick="loggedStatus()">MY ACCOUNT</a></li>
-            <li><a href="fees.html">FEES</a></li>
-            <li><a href="aboutus.html">ABOUT US</a></li>
+            <li><a href="fees.php">FEES</a></li>
+            <li><a href="aboutus.php">ABOUT US</a></li>
             <li><a class="active" href="index.php">HOME</a></li>
         </ul>
     </nav>
@@ -41,26 +41,32 @@
     <div class="scroll_auto">
         <ul class="scroll_content">
         <?php
-            $sort_array = [];
-            $fs = fopen('stores.csv', 'r');
-            while($sort_element = fgetcsv($fs)){
-                $sort_array [] = $sort_element;
-            }
+            //Working on id
+            session_start();
+            require 'stores_function.php';
+            $stores = read_all_stores();
+
+            //Function use to sort
             function cmp_date_store($s1, $s2) {
-                return strtotime($s2[3]) - strtotime($s1[3]);
+                return strtotime($s2['created_time']) - strtotime($s1['created_time']);
               }
-            usort($sort_array, 'cmp_date_store');
+
+            //Sort base on created-date
+            usort($stores, 'cmp_date_store');
             
+            //Display the the stores base on the sort order and get the store's ID
             $count = 0;
-            foreach ($sort_array as $entry){ 
+            foreach ($stores as $s){ 
                 if ($count >= 10) {
                     break;
-                }             
+                }
+                $id = $s['id']; //Get the store's ID            
                 echo'<li>';
-                echo'<a href="./newstore1.html"><img src="./image/logo-store1.jpg" alt="Computer Tech" id="s1"></a>';
+                echo"<a href='newstore1.php?store=$id'>";
+                echo'<img src="./image/logo-store1.jpg" alt="Computer Tech" id="s1"></a>';
                 echo'<div>';
                 echo'<p>';
-                printf("%s", $entry[1]);
+                printf("%s", $s['name']); //Display the name of the suitable store
                 echo'</p>';
                 echo'</div>';
                 echo'</li>'; 
@@ -74,93 +80,114 @@
     <div class="scroll_auto">
         <ul class="scroll_content1">
         <?php
-            $sort_product_array = [];
-            $fp = fopen('products.csv', 'r');
-            while($sort_product_element = fgetcsv($fp)){
-                $sort_product_array [] = $sort_product_element;
-            }
+        //Working on ID
+        require 'product_functions.php';
+        $products = read_all_products();
+
+        //Sort function
             function cmp_date_product($p1, $p2) {
-                return strtotime($p2[3]) - strtotime($p1[3]);
+                return strtotime($p2['created_time']) - strtotime($p1['created_time']);
               }
-            usort($sort_product_array, 'cmp_date_product');
-            $check = 1;
-            $count = 0;
-            foreach ($sort_product_array as $entry){
+
+            usort($products, 'cmp_date_product');//Sort base on created_time
+
+            $check = 1;//Element use to change the picture
+            $count = 0;//Count to create the number of element
+            foreach ($products as $p){
                 if($count < 10){
+                    $id = $p['id'];
                     echo'<li>';
-                    if($check == 1 ){
-                        echo'<a href="./product.html"><img src="./image/store-product/cat5-product2.jpg"  alt="product jpg" id="p1"></a>';
+                    if($check == 1 )//Picture for product 1
+                    {
+                        echo"<a href='./product.php?product=$id'>";
+                        echo'<img src="./image/store-product/cat5-product2.jpg"  alt="product jpg" id="p1"></a>';
                         $check++;
                         }
-                    else{
-                        echo'<a href="./product2.html"><img src="./image/store-product/cat4-product4.jpg"  alt="product jpg" id="p2"></a>';
+                    else //Picture for product 2
+                    {
+                        echo"<a href='./product2.php?product=$id'>";
+                        echo'<img src="./image/store-product/cat4-product4.jpg"  alt="product jpg" id="p2"></a>';
                         $check = 1;
                         } 
                     echo'<div>';
                     echo'<p>';
-                    printf("%s", $entry[1]);
+                    printf("%s", $p['name']);//Display the name of the suitable product
                     echo'</p>';
                     echo'</div>';
                     echo'</li>'; 
-                    $count++;
+                    $count++;//Make count value increase 1 per loop
                 }
             }
             ?>
         </ul>
     </div>
+
     <section align="center"><header>FEATURED STORES</header></section>
     <div class="display_auto">
         <div class="display_content" >
             <?php
-            $fd = fopen('stores.csv','r');
-            $count = 0;
-            while($i = fgetcsv($fd)){
-                if($i[4] == 'TRUE'){
-                    if($count < 10){
-                        echo'<a href="./newstore1.html">';
+
+            $count = 0;//Count to create the number of element
+
+            //Loop for create 10 featured stores
+            foreach ($stores as $s){
+                if($s['featured'] == 'TRUE')//Only show the 'TRUE' fetured store
+                {
+                    if($count < 10)//Only show 10 featured stores
+                    {
+                        $id = $s['id']; //Get the value from array
+                        echo"<a href='./newstore1.php?store=$id'>";//Show id on URL
                         echo'<img src="./image/logo-store1.jpg"  alt="Computer Tech" id="f1">';
                         echo'<p>';
-                        printf("%s", $i[1]);
+                        printf("%s", $s['name']);//Display the name of the suitable product
                         echo'</p>';
                         echo'</a>';
-                        $count++;
+                        $count++; //Make count value increase 1 per loop
                     }
                 }
             }
             ?>
         </div>
     </div>
+
     <section align="center"><header>FEATURED PRODUCTS</header></section>
     <div class="display_auto">
         <div class="display_content" >
         <?php
-            $fp = fopen('products.csv', 'r');
-            $check = 1;
-            $count = 0;
-            while ($i = fgetcsv($fp)){
-                if($i[5] == 'TRUE'){
-                    if($count < 10){
+
+            $check = 1;//Element use to change the picture
+            $count = 0;//Count to create the number of element
+
+            foreach ($products as $p){
+                if($p['featured_in_mall'] == 'TRUE')//Only show the 'TRUE' fetured product
+                {
+                    if($count < 10)//Only show 10 featured stores
+                    {
+                        $id = $p['id'];//Get the value from array
                         if($check == 1 ){
-                            echo'<a href="./product.html"><img src="./image/store-product/cat5-product2.jpg"  alt="product jpg" id="p1">';
+                            echo"<a href='./product.php?product=$id'>";//Show id on URL
+                            echo'<img src="./image/store-product/cat5-product2.jpg"  alt="product jpg" id="p1">';
                             echo'<p>';
-                            printf("%s", $i[1]);
+                            printf("%s", $p['name']);//Display the name of the suitable product
                             echo'</p>';
                             echo'</a>';
                             $check++;
                             }
                         else{
-                            echo'<a href="./product2.html"><img src="./image/store-product/cat4-product4.jpg"  alt="product jpg" id="p2">';
+                            echo"<a href='./product2.php?product=$id'>";
+                            echo'<img src="./image/store-product/cat4-product4.jpg"  alt="product jpg" id="p2">';
                             echo'<p>';
-                            printf("%s", $i[1]);
+                            printf("%s", $p['name']);//Display the name of the suitable product
                             echo'</p>';
                             echo'</a>';
                             $check = 1;
                             } 
-                    $count++;
+                    $count++;//Make count value increase 1 per loop
                     }
                 }
             }
-            ?>        </div>
+            ?>        
+        </div>
     </div>
 </main>
 
@@ -174,9 +201,9 @@
     <div id="botnav">
         <nav>
             <ul>
-                <li><a href="privacy.html">Privacy Policy</a></li>
-                <li><a href="tos.html">Terms of Service</a></li>
-                <li><a href="copyright.html">Copyright</a></li>
+                <li><a href="privacy.php">Privacy Policy</a></li>
+                <li><a href="tos.php">Terms of Service</a></li>
+                <li><a href="copyright.php">Copyright</a></li>
             </ul>
         </nav>
     </div>
---
+++ product2.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Product</title>
+    <link rel="stylesheet" href="navigation.css" type="text/css"/>
+    <link rel="stylesheet" href="./Assist/product.css">
+    <link rel="stylesheet" href="./Assist/app.css" type="text/css">
+    <link rel="stylesheet" href="./Assist/icon.css" type="text/css"/>
+</head>
+<body onpageshow="popup()">
+<header>
+    <div class="logo-home">
+        <a href="./index.php"><img src="./image/mall-logo.jpg" alt="shopping cart creative"></a>
+    </div>
+<div id="topnav">
+    <nav>
+         <ul>
+            <li><a href="order.php">ORDERS</a></li>
+            <li><a href="contacts.php">CONTACTS</a></li>
+            <li><a href="faqs.php">FAQS</a></li>
+            <li class="dropdown">
+                <a class="dropbutton">BROWSE</a>
+                <div class="drop-content">
+                    <a href="browse_name.php">Browse Stores by Names</a>
+                    <a href="browse_cat.php">Browse Stores by Category </a>
+                </div>
+            </li>
+            <li><a href="account.php">MY ACCOUNT</a></li>
+            <li><a href="fees.php">FEES</a></li>
+            <li><a href="aboutus.php">ABOUT US</a></li>
+            <li><a href="index.php">HOME</a></li>
+         </ul>
+     </nav>
+</div>
+</header>
+
+<main>
+    <div class="product-detail">
+    <?php
+                session_start();
+                require 'product_functions.php';
+
+                $id = $_GET['product'];
+
+                $product = get_product($id);
+ 
+        echo'<h1>';
+        printf("%s", $product['name']);
+        echo'</h1>';
+        echo'<div class="image-infor">';
+            echo'<div class="manage-image">';
+                
+                echo'<img class="main-image" src="./image/store-product/cat4-product4.jpg" alt="Laptop gaming Acer Aspire 7 A715 42G R4ST">';
+                echo'<div class="show-more-image">';
+                    echo'<img src="./image/store-product/cat1-product1-pic2.png" alt="">';
+                    echo'<img src="./image/store-product/cat1-product1-pic3.png" alt="">';
+                    echo'<img src="./image/store-product/cat1-product1-pic4.png" alt="">';
+                    echo'<img src="./image/store-product/cat1-product1-pic5.png" alt="">';
+                echo'</div>';
+            echo'</div>';
+            echo'<ul>';
+                echo'<li><h3> Manufacturer : Acer</h3></li>';
+                echo'<li><h3> Guarantee : 12 month</h3></li>';
+                echo'<li><h3> Price : ';
+                printf("%s", $product['price']);
+                echo' USD</h3></li>';
+                echo'<li><h3> Detailed evaluation : </h3></li>';
+                echo'<ol>';
+                    echo'<li><h4>BEST LAPTOP GAMING COMPLEX</h4></li>';
+                    echo"<p>Acer Aspire 7 2020 A715 42G R4ST with integrated NVIDIA GTX1650 4 GB GDDR6 graphics card launched in 2020, is the best gaming laptop in the segment. Not only that, this version also brings a new neat and sexy design. Aspire 7 2020 is equipped with the most powerful cooling system in the segment, inheriting technology from Acer's more advanced models, with top-notch configuration, allowing users to play games while working at any time.</p>";
+                    echo'<li><h4>ULTIMATE GAME EXPERIENCE DESIGN</h4></li>';
+                    echo'<p>The system of two radiator fans, 3 full-size copper pipes, and a large-size heatsink helps the Acer Aspire 7 A715 2020 optimize the heat dissipation capacity. The solid design hinge is less shaking when playing, capable of opening 180 degrees to better protect the machine.</p>';
+                    echo'<li><h4>3S1 WARRANTY SERVICES</h4></li>';
+                    echo"<p>3S1 is Acer's quick warranty for gamers. Aspire 7 enjoy superfast 3S1 Warranty: fast warranty, within 3 days including Saturday and Sunday, more than 3 days 1 for 1 new product with equal or higher value.</p>";
+                echo'</ol>';
+                echo'<div class="buy-add">';
+                    echo'<button class="buy"><a href="">BUY NOW</a></button>';
+                    echo'<button class="add" onClick="Addedtocart()">ADD</button>';
+                echo'</div>';
+            echo'</ul>';
+        echo'</div>';
+        ?>
+    </div>
+
+    <div class="technical-parameters">
+        <h2>Technical parameters:</h2>
+        <table>
+            <tr>
+                <th>CPU</th>
+                <td>AMD Ryzen 5 – 4600H</td>
+            </tr>
+            <tr>
+                <th>RAM</th>
+                <td>DDR4 8GB (1 x 8GB) 3200MHz; 2 slots, up to 32GB</td>	
+            </tr>
+            <tr>
+                <th>Hard Drive</th>
+                <td>512GB SSD NVMe M.2 PCIe Gen 3 x 2</td>
+            </tr>
+            <tr>
+                <th>Graphics card</th>
+                <td>NVIDIA GeForce GTX 1650 4GB GDDR6</td>
+            </tr>
+            <tr>
+                <th>Screen</th>
+                <td>15.6" FHD (1920 x 1080) IPS, Anti-Glare, 60Hz</td>
+            </tr>
+            <tr>
+                <th>Connector</th>
+                <td><ul style="list-style-type: none;">
+                    <li>1x USB 3.0</li>
+                    <li>1x USB Type C</li>
+                    <li>2x USB 2.0</li>
+                    <li>1x HDMI</li>
+                    <li>1x RJ45</li>
+                </ul></td>
+            </tr>
+            <tr>
+                <th>Optical drive</th>
+                <td>None</td>
+            </tr>
+            <tr>
+                <th>Audio</th>
+                <td>True Harmony; Dolby® Audio Premium</td>
+            </tr>
+            <tr>
+                <th>Read the memory card</th>
+                <td>None</td>
+            </tr>
+            <tr>
+                <th>LAN standard</th>
+                <td>10/100/1000/Gigabits Base T</td>
+            </tr>
+            <tr>
+                <th>WIFI standard</th>
+                <td>Wi-Fi 6(Gig+)(802.11ax) (2x2)</td>
+            </tr>
+            <tr>
+                <th>Bluetooth</th>
+                <td>v5.0</td>
+            </tr>
+            <tr>
+                <th>Webcam</th>
+                <td>HD Webcam</td>
+            </tr>
+            <tr>
+                <th>Operating system</th>
+                <td>Windows 10 Home</td>
+            </tr>
+            <tr>
+                <th>Battery</th>
+                <td>4 Cell 48Whr</td>
+            </tr>
+            <tr>
+                <th>Weight</th>
+                <td>2.4 kg</td>
+            </tr>
+            <tr>
+                <th>Color</th>
+                <td>Black, with keyboard light</td>
+            </tr>
+            <tr>
+                <th>Size</th>
+                <td>363.4 x 254.5 x 23.25 (mm)</td>
+            </tr>
+        </table>
+    </div>
+
+    <div class="recommended-product">
+        <div class="name-tag">
+            <h2>Recommended Products:</h2>
+        </div>
+
+        <div class="image-tags">
+            <div class="set-collum">
+                <a href="./product.php"><img src="./image/store-product/cat1-product1.png" alt=""></a>
+                <a href="./product.php"><h5>Laptop gaming Acer Aspire 7 A715 42G R4ST</h5></a>
+            </div>
+            <div class="set-collum">
+                <a href="./product.php"><img src="./image/store-product/cat1-product3.png" alt=""></a>
+                <a href="./product.php"><h5>Laptop ASUS TUF Gaming F15 FX506LH HN002T</h5></a>
+            </div>
+            <div class="set-collum">
+                <a href="./product.php"><img src="./image/store-product/cat1-product4.jpg" alt=""></a>
+                <a href="./product.php"><h5>Laptop gaming Acer Nitro 5 AN515 55 5923</h5></a>
+            </div>
+            <div class="set-collum">
+                <a href="./product.php"><img src="./image/store-product/cat1-product5.png" alt=""></a>
+                <a href="./product.php"><h5>Laptop Gaming MSI GF63 Thin 10SC 014VN</h5></a>
+            </div>
+            <div class="set-collum">
+                <a href="./product.php"><img src="./image/store-product/cat1-product6.jpg" alt=""></a>
+                <a href="./product.php"><h5>Laptop Gaming Asus ROG Strix G G512 IAL013T</h5></a>
+            </div>
+        </div>
+    </div>
+</main>
+
+<div id="botnav">
+    <nav>
+        <ul>
+            <li><a href="privacy.php">Privacy Policy</a></li>
+            <li><a href="tos.php">Terms of Service</a></li>
+            <li><a href="copyright.php">Copyright</a></li>
+        </ul>
+    </nav>
+</div>
+<div id="consentPopup" class="hidden">
+    <p>This site uses cookies to improve the user experience. By clicking
+        here you accept cookies placed by our site.</p> <a id="accept" href="#">Learn More</a>
+    <button onclick="consent()">I Consent</button>
+</div>
+</body>
+</html>
+<script src="./Assist/app.js"></script>
+<script src="cart.js"></script>
